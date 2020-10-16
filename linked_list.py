ae9c0c78eb0c260596ea8b2dba4852ab8de2dbb7
DanielbDEV <daniel.ba.96.59@gmail.com>
2020-10-16 12:00:39 +0200

    Fixed insert and __repr__ function. Renamed class to "Node"

--- linked_list.py
+++ linked_list.py
@@ -1,20 +1,24 @@
-class LinkedList():
+class Node():
 
-    def __init__(self, key, data):
+    def __init__(self, key, data, pointer=None):
         self.key = key
         self.data = data
-        self.pointer = None
+        self.pointer = pointer
 
     def __repr__(self):
-        str_1 = self.get(str(self.key))
-        #str_1 = str_1 + str(self.pointer.get())
-        return "__repr__ to be implemented..."
+        list_ = [self.get(self.key)]
+        pointer = self.pointer
+        while pointer.pointer is not None:
+            list_.append(pointer.get(pointer.key))
+            pointer = pointer.pointer
+        list_.append(pointer.get(pointer.key))
+        return str(list_)
 
     def get(self, key):
         if self.key == key:
             return self.data
         elif self.pointer is None:
-            return "end of list"
+            return False
         else:
             return self.pointer.get(key)
 
@@ -22,18 +26,14 @@ class LinkedList():
         if self.key == key:
             self.data = data
 
-        # elif self.pointer.key > key:
-        #    old_pointer = self.pointer
-        #    self.pointer = LinkedList(key, data)
-        #    self.pointer.insert(old_pointer)
-
         elif self.key > key:
-            self.pointer = self
+            new_node = Node(self.key, self.data, self.pointer)
+            self.pointer = new_node
             self.key = key
             self.data = data
 
         elif self.pointer is None:
-            self.pointer = LinkedList(key, data)
+            self.pointer = Node(key, data)
 
         else:
             self.pointer.insert(key, data)
@@ -41,7 +41,7 @@ class LinkedList():
 
 def construct_ll(list_, root_element=None):
     if root_element is None:
-        root_element = LinkedList(list_[0].key, list_[0].data)
+        root_element = Node(list_[0].key, list_[0].data)
     #list_ = list_[1:]
     for i in list_[1:]:
         root_element.insert(i.key, i.data)
@@ -67,7 +67,13 @@ def create_ll(n):
 
 
 ll = create_ll(5)
-print(ll.key)
 print(ll)
 # list_ = [[1,"A"], [2,"B"], [3,"C"], [4,"D"], [5,"E"]]
 # create_linked_list(list_)
+
+elements_to_insert =
+
+ll.insert(2,"X")
+ll.insert(4,"Y")
+ll.insert(3.5,"3.5")
+print(ll)
